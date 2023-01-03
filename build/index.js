db49bc0fc31c53d47406f7d3d792b1e3c1474ed4
Ikebonamin <44932080+Ikebonamin@users.noreply.github.com>
2023-01-03 15:35:21 -0300

    Delete build directory

--- build/index.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const dataBase_1 = require("./dataBase");
-console.log("All users from user: ", dataBase_1.user[1].id);
-//# sourceMappingURL=index.js.map
